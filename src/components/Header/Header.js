6d3be2e60c0478df895de12a9e9f5b726ef6dd24
preet-singh <preetsingh13@gmail.com>
2019-11-20 18:24:55 -0600

    styling update, logo problem - to fix tomorrow

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -41,7 +41,7 @@ class Header extends Component {
   render() {
     return (
       <header>
-        <h1>
+        <h1 className="logo">
           <Link to='/'>
             Hola!
           </Link>
