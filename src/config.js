591b935c6a2cac8b04c2d0dc5a1ede959c43f4d1
preet-singh <preetsingh13@gmail.com>
2019-11-22 15:05:53 -0600

    .

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: (process.env.NODE_ENV === 'production' 
+    ? 'https://quiet-earth-85650.herokuapp.com/api' : 'http://localhost:8000/api'),
   TOKEN_KEY: 'blogful-client-auth-token',
 }
+//http://localhost:8000/api
