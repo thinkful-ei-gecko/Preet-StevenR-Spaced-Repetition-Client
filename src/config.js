81090ba902a1bcfce238f8707bcef8ded1dda17b
preet-singh <preetsingh13@gmail.com>
2019-12-03 22:21:17 -0600

    fudu deployment issues

--- src/config.js
+++ src/config.js
@@ -1,14 +1,7 @@
-let apiPath;
-let tokenKey;
-if (process.env.NODE_ENV === 'production') {
-  apiPath = 'https://shielded-depths-44236.herokuapp.com/api'
-  tokenKey = 'blogful-client-auth-token'
-} else {
-  apiPath = 'http://localhost:8000/api'
-  tokenKey = 'blogful-client-auth-token'
-}
 
 export default {
-  API_ENDPOINT: apiPath,
-  TOKEN_KEY: tokenKey,
+  API_ENDPOINT: 'https://shielded-depths-44236.herokuapp.com/api',
+  TOKEN_KEY: 'blogful-client-auth-token'
 }
+//'http://localhost:8000/api'
+//
